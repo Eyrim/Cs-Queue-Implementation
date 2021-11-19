5659620f3d055fb94d0e4030b5123058cc61f95d
Eyrim <62777325+Eyrim@users.noreply.github.com>
2021-11-19 15:01:51 +0000

    Refactor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace LinearQueue
 {
@@ -7,25 +7,6 @@ namespace LinearQueue
         static void Main(string[] args)
         {
             LinearQueue<string> queue = new LinearQueue<string>(MaxSize:10);
-            
-            queue.AddValue("1");
-            queue.AddValue("2");
-            queue.AddValue("3");
-            queue.AddValue("4");
-            queue.AddValue("5");
-            queue.AddValue("6");
-            queue.AddValue("7");
-            queue.AddValue("8");
-            queue.AddValue("9");
-            queue.AddValue("10");
-            
-            Console.WriteLine(queue.RemoveValue());
-
-            queue.AddValue("Question Mark");
-
-            Console.WriteLine(queue.RemoveValue());
-
-            Console.ReadKey();
         }
     }
 }
